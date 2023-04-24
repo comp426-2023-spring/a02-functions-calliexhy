b17574d5f18a38e11529166a40cc97909f069cd0
calliexhy <69410421+calliexhy@users.noreply.github.com>
2023-04-24 01:45:29 -0400

    Updated

--- cli.js
+++ cli.js
@@ -11,15 +11,35 @@ if(argv.h) {
     process.exit(0);
 }
 
-const timezone = moment.tz.guess();
+let latitude, longitude
 
-const data = await response.json();
+if (args.n) {
+    latitude = args.n;
+} else if (args.s) {
+    latitude = -args.s;
+} else {
+    console.log("Latitude must be in range");
+    process.exit(0);
+}
+
+if (args.e) {
+    longitude = args.e;
+} else if (args.w) {
+    longitude = -args.w;
+} else {
+    console.log("Longitude must be in range");
+    process.exit(0);
+}
+
+let timezone = moment.tz.guess();
+
+let data = await response.json();
 if (args.j) {
     console.log(data);
     process.exit(0);
 }
 
-const days = args.d 
+let days = args.d 
 
 if (days == 0) {
   console.log("today.")
@@ -27,4 +47,20 @@ if (days == 0) {
   console.log("in " + days + " days.")
 } else {
   console.log("tomorrow.")
+}
+
+let request_url = "https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&daily=precipitation_hours&current_weather=true&timezone=" + timezone;
+let response = await fetch(request_url);
+
+
+if (days==0) {
+  if (precipitation_hours_data[1] > 0) {
+      console.log('You might need your galoshes ' + day_str);
+  } else {
+      console.log('You will not need your galoshes ' + day_str);
+  }
+} else if (precipitation_hours_data[days] > 0) {
+  console.log('You might need your galoshes ' + day_str);
+} else {
+  console.log('You will not need your galoshes ' + day_str);
 }
